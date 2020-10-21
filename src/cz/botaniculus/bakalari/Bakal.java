3736773cd30fcc2fbbd51c26105c4aa7a2f57ecd
Botaniculus <bofin@skaut.cz>
2020-10-21 14:07:11 +0200

    aaa

--- src/cz/botaniculus/bakalari/Bakal.java
+++ src/cz/botaniculus/bakalari/Bakal.java
@@ -6,72 +6,84 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.*;
+
+
 public class Bakal {
-    private String[] tokens;
-    private String baseURL = "https://www.gvp.cz/info";
+    final private String baseURL = "https://www.gvp.cz/info";
     private URL targetURL;
     private String data;
+    private String got;
+    private String output;
+    private String accessToken;
+    private String refreshToken;
 
-    private int accessToken=0;
-    private int refreshToken=3;
-////////////////////////////////////////////////////////////////////////////////////
-    public String[] login(String username, String password) throws IOException {
-        data = "client_id=ANDR&grant_type=password&username=" + username + "&password=" + password;
+    public void login(String username, String password, boolean refresh) throws IOException {
+        if(!refresh) {
+            data = "client_id=ANDR&grant_type=password&username=" + username + "&password=" + password;
+        }
+        else if(refresh){
+            data = "client_id=ANDR&grant_type=refresh_token&refresh_token="+refreshToken;
+        }
         targetURL=new URL(baseURL+"/api/login");
+        got=this.bakalari(targetURL, "POST", data, null);
+        JSONObject obj = new JSONObject(got);
+        accessToken = obj.getString("access_token");
+        refreshToken = obj.getString("refresh_token");
+    }
 
-        String output = "";
-        HttpURLConnection conn = (HttpURLConnection)targetURL.openConnection();
+    public String userInfo() throws IOException {
+        targetURL=new URL(baseURL+"/api/3/user");
+        got=this.bakalari(targetURL, "GET", null, accessToken);
+
+        JSONObject obj = new JSONObject(got);
+        String FullName = obj.getString("FullName");
+        return FullName;
+    }
+    public String timetable(int day, int month, int year) throws IOException {
+        targetURL=new URL(baseURL+"/api/3/timetable/actual?date="+year+"-"+month+"-"+day);
+        got=this.bakalari(targetURL, "GET", null, accessToken);
+        /*JSONObject obj = new JSONObject(output);
+        JSONArray Days = obj.getJSONArray("Days");
+        JSONArray Atoms = Days.getJSONArray(0);
+        JSONObject objAtoms = Atoms.getJSONObject(0);
+        String[] id = new String[objAtoms.length()];
+        String[] theme = new String[objAtoms.length()];
+        for(int i=0; i< Atoms.length(); i++){
+            id[i]=objAtoms.getString("HourId");
+            theme[i]=objAtoms.getString("Theme");
+
+        }
+        for(int i = 0; i< Atoms.length(); i++){
+            System.out.println(id[i]+" " + theme[i]);
+        }*/
+        return got;
+    }
+    private String bakalari(URL target, String method, String data, String token) throws IOException {
+        output=null;
+        HttpURLConnection conn = (HttpURLConnection)target.openConnection();
         conn.setDoOutput(true);
         conn.setInstanceFollowRedirects(true);
-        conn.setRequestMethod("POST");
         conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         conn.setRequestProperty("charset", "UTF-8");
-        conn.setRequestProperty("Content-Length", "" + Integer.toString(data.length()));
-        conn.setUseCaches(false);
-        try(OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream())) {
-            out.write(data);
+        if(token!=null) {
+            conn.setRequestProperty("Authorization", "Bearer " + token);
         }
-        try(BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-            String currentLine;
-            while((currentLine = in.readLine()) != null) {
-                output+=currentLine;
+        conn.setRequestMethod(method);
+        conn.setUseCaches(false);
+        if(data!=null){
+            conn.setRequestProperty("Content-Length", "" + Integer.toString(data.length()));
+            try(OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream())) {
+                out.write(data);
             }
         }
-        output = output.replace("null", "");
-        output = output.replace("{", "");
-        output = output.replace("}", "");
-        String[] outputAr=output.split(",");
-        tokens =new String[outputAr.length];
-        for (int i = 0; i < outputAr.length; i++){
-            outputAr[i]=outputAr[i].replace("\"", "");
-            String[] slup = outputAr[i].split(":");
-            tokens[i]=slup[1];
-        }
-
-        return tokens;
-    }
-/////////////////////////////////////////////////////////////////////////////////////////
-
-    public String timetable() throws IOException {
-        System.out.println(tokens[accessToken]);
-        String output = "";
-        targetURL=new URL(baseURL+"/api/3/timetable/actual?date=2020-10-20");
-        HttpURLConnection conn = (HttpURLConnection)targetURL.openConnection();
-        conn.setDoOutput(true);
-        conn.setInstanceFollowRedirects(true);
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("charset", "UTF-8");
-
-        conn.setRequestProperty("Authorization","Bearer "+ tokens[accessToken]);
-        conn.setRequestMethod("GET");
-        conn.setUseCaches(false);
-
         try(BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
             String currentLine;
             while((currentLine = in.readLine()) != null){
                 output+=currentLine;
             }
         }
+        output=output.substring(4);
         return output;
     }
 }
--- src/cz/botaniculus/bakalari/Main.java
+++ src/cz/botaniculus/bakalari/Main.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in, "Windows-1250");
+
         Bakal bakal = new Bakal();
         //Set master parameters
         System.out.print("Username: ");
         String username = scanner.nextLine();
         System.out.print("Password: ");
         String password = scanner.nextLine();
+
         //login
-        bakal.login(username, password);
+        bakal.login(username, password, false);
+
+        //user info
+        System.out.println(bakal.userInfo());
 
         //timetable
-        System.out.println(bakal.timetable());
+        System.out.println(bakal.timetable(21, 10, 2020));
+
+
     }
 }
